bd34e16efb02eae653f61398367293a6e71ddf34
BalrajDoddi <balraj.doddi@outlook.com>
2021-05-17 20:39:20 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,98 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+const app = express();
+
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Balraj Doddi',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Balraj Doddi',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    helpText: 'This is some helpful text.',
+    name: 'Balraj Doddi',
+  });
+});
+
+app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address',
+    });
+  }
+
+  // console.log(req.query.address);
+  // res.send({
+  //   forecast: 'Smoke.',
+  //   location: req.query.address,
+  // });
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        return res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
+      });
+    }
+  );
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Balraj Doddi',
+    errorMessage: 'Help article not found.',
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Balraj Doddi',
+    errorMessage: 'Page not found.',
+  });
+});
+
+const PORT = process.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
